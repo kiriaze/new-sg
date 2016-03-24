2955fb0855fa5ebf21f8d4b3c1a908fa70f17db7
Constantine <ckiriaze@gmail.com>
2016-03-24 06:34:05 -0700

    updated settings, renamed new_grid scss to xyz ext to exclude from compiling temp

--- src/assets/js/modules.js
+++ src/assets/js/modules.js
@@ -1,3 +1,5 @@
+require("../../modules/00_settings/script.js");
 require("../../modules/01_introduction/script.js");
 require("../../modules/07_forms/script.js");
 require("../../modules/08_media/script.js");
+require("../../modules/99_shame/script.js");
